aab8554246aa0b168dd0e1fb7898bfc4e7b01032
Gregory Sims <briggybros.2@googlemail.com>
2018-10-29 12:39:21 +0000

    Lab 3 start

--- tensorboard.sh
+++ tensorboard.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-ssh bluecrystal -L 6006:$1
-
 URL="http://localhost:6006"
 
 if which xdg-open > /dev/null
 then
-  xdg-open $URL
+  xdg-open $URL > /dev/null 2> /dev/null &
 elif which gnome-open > /dev/null
 then
-  gnome-open $URL
+  gnome-open $URL > /dev/null 2> /dev/null &
 fi
+
+ssh bluecrystal -L 6006:$1
