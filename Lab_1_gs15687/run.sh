4ec49dc26e4d774fe75f40b7dffb2b38001cc760
Gregory Sims <briggybros.2@googlemail.com>
2018-10-29 12:13:00 +0000

    Lab 2

--- Lab_1_gs15687/run.sh
+++ Lab_1_gs15687/run.sh
@@ -1,1 +1,14 @@
-python $1 & tensorboard --logdir=logs/ --port=$2
+#!/bin/bash
+
+module add libs/tensorflow/1.2
+
+let ipnport=($UID-6025)%65274
+
+ipnip=$(hostname -i)
+
+echo "Copy this as the parameter from tensorboard.sh locally"
+echo "$ipnip:$ipnport"
+
+read -n 1 -s -r -p "Press any key to start"
+
+python $1 & tensorboard --logdir=logs/ --port=$ipnport
--- tensorboard.sh
+++ tensorboard.sh
@@ -1,1 +1,13 @@
-ssh bluecrystal -L 6006:$1:$2
+#!/bin/bash
+
+ssh bluecrystal -L 6006:$1
+
+URL="http://localhost:6006"
+
+if which xdg-open > /dev/null
+then
+  xdg-open $URL
+elif which gnome-open > /dev/null
+then
+  gnome-open $URL
+fi
